49cea79552d5330a18498c30ab94b0865b1eb331
Andrew Levin <andrewlevingit@hotmail.com>
2024-12-07 22:59:48 +0300

    2024-12-07 22:59:48 N

--- src/telepostkeeper.py
+++ src/telepostkeeper.py
@@ -1,17 +1,15 @@
 
-import json
 import pathlib
 import pprint
 import sys
-from datetime import date, datetime
+from datetime import datetime
 import asyncio
+from typing import Optional
 
-from aiogram.types import PhotoSize
 from dotenv import load_dotenv
 import os
 import yaml
-from telegram import Update, Video, Document, Audio, Message
-from telegram._files._basethumbedmedium import _BaseThumbedMedium
+from telegram import Update, Video, Document, Audio, Message, Chat, PhotoSize
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
 ENV_NAME_BOT_TOKEN = 'TELEPOSTKEEPER_BOT_TOKEN'
@@ -35,120 +33,94 @@ store.mkdir(parents=True, exist_ok=True)
 channels_list = [int(item) for item in os.getenv(ENV_NAME_CHANNELS, '').strip().split(',') if item.isdigit()]
 
 
-
-async def update_chat_about_info(full_name: str, chat_dir: pathlib.Path):
-    print('🦠 update_chat_about: ')
+async def update_chat_about_info(chat: Chat, chat_dir: pathlib.Path):
+    about_path = chat_dir / f'about.yaml'
 
     last_full_name = ''
-    about_yaml = chat_dir / f'about.yaml'
-
-    if about_yaml.exists():
+    if about_path.exists():
         try:
-            with about_yaml.open("r", encoding="utf-8") as f:
+            with about_path.open("r", encoding="utf-8") as f:
                 data = yaml.safe_load(f) or {}
             last_full_name = data.get("full_name", "")
         except yaml.YAMLError as e:
-            print("Failed to load YAML from %s: %s", about_yaml, e)
+            print("Failed to load YAML from %s: %s", about_path, e)
         except Exception as e:
-            print("Unexpected error reading %s: %s", about_yaml, e)
+            print("Unexpected error reading %s: %s", about_path, e)
 
-    if last_full_name != full_name:
-        print('🧬 rename')
+    if last_full_name != chat.full_name:
         try:
-            data = {"full_name": full_name}
-            with about_yaml.open("w", encoding="utf-8") as f:
+            data = {"full_name": chat.full_name}
+            with about_path.open("w", encoding="utf-8") as f:
                 yaml.dump(data, f, allow_unicode=True, default_flow_style=False)
-            print("Updated index.yaml with new full_name: %s", full_name)
+            print("Updated index.yaml with new full_name: %s", chat.full_name)
         except yaml.YAMLError as e:
-            print("Failed to write YAML to %s: %s", about_yaml, e)
+            print("Failed to write YAML to %s: %s", about_path, e)
         except Exception as e:
-            print("Unexpected error writing %s: %s", about_yaml, e)
+            print("Unexpected error writing %s: %s", about_path, e)
 
 
-def get_real_chat_id(chat_id_raw):
-    return -chat_id_raw - 1000000000000
-
-async def empty_task():
-    pass
-
-async def make_thumbnail(thumbnail, root_path, post_id):
-    context = dict()
-
-    context['thumbnail_file_size'] = thumbnail.file_size
-    context['thumbnail_height'] = thumbnail.height
-    context['thumbnail_width'] = thumbnail.width
-
-    thumb_file = await thumbnail.get_file()
-    thumb_path = pathlib.Path(thumb_file.file_path)
-    thumb_store_path = root_path / f'{post_id}-thumbnail{thumb_path.suffix}'
-
-    context['thumbnail_path'] = thumb_store_path.as_posix()
-
-    try:
-        task_created = thumb_file.download_to_drive(thumb_store_path)
-    except Exception as e:
-        print('Exception in Thumb')
-        task_created = empty_task()
-
-    return context, task_created
+def get_real_chat_id(chat_id_raw: int) -> int:
+    return - chat_id_raw - 1000000000000
 
 
-def get_extension(media_obj: Video | Audio | Document | PhotoSize) -> str:
-    ext = '.file'
+async def get_extension_media_heavy_object(media_type: str, media_obj: Video | Audio | Document | PhotoSize) -> str:
+    if media_type == 'photo':
+        return '.jpg'
 
-    print('🔬 Extension')
-    pprint.pprint(media_obj)
-    print()
-    print('media_obj.mime_type: ', media_obj.mime_type)
-    print()
+    if hasattr(media_obj, 'file_name') and media_obj.file_name:
+        try:
+            ext = pathlib.Path(media_obj.file_name).suffix
+        except Exception as e:
+            print(f'🎸 Error {e}')
+            ext = ''
 
-    if hasattr(media_obj, 'file_name'):
-        if media_obj.file_name:
-            try:
-                ext = pathlib.Path(media_obj.file_name).suffix
-            except Exception as e:
-                print(f'🎸 Errro {e}')
-
-    if ext != '.file':
         return ext
 
-    if hasattr(media_obj, 'mime_type'):
-        print('media_obj.mime_type: ', media_obj.mime_type)
-        if media_obj.mime_type:
+    if hasattr(media_obj, 'mime_type') and media_obj.mime_type:
             try:
                 ext = media_obj.mime_type.split('/')[-1]
                 ext = f'.{ext}'
             except Exception as e:
-                print(f'🎸 Errro {e}')
+                print(f'🎸 Error {e}')
+                ext = ''
 
-    return ext
-
-
-async def make_file_download(media_obj_type: str, media_obj: any, file_path: pathlib.Path):
-    print('Download: ')
-    pprint.pprint(media_obj)
-    print()
-
-    if media_obj_type == 'photo':
-        media_obj = media_obj[-1]
-        print('Photo ')
-        pprint.pprint(media_obj)
-
-    if not hasattr(media_obj, 'get_file'):
-        return
+            return ext
 
     try:
         _file = await media_obj.get_file()
-        await _file.download_to_drive(file_path)
+
+        if hasattr(_file, 'file_path') and _file.file_path:
+            try:
+                _path = pathlib.Path(_file.file_path)
+                ext = _path.suffix
+            except Exception as e:
+                print(f'Error {e}')
+                ext = ''
+
+            return ext
+
     except Exception as e:
-        print(f'🍎 Error Doanloading: \t {e}')
+        print(f'Error {e}')
+
+    return ''
+
+
+async def make_file_download(media_obj: any, path_media_obj: pathlib.Path):
+    try:
+        _file = await media_obj.get_file()
+        await _file.download_to_drive(path_media_obj)
+    except Exception as e:
+        print(f"Error downloading file to {path_media_obj}: {e}")
+        return
+
+    return path_media_obj
 
     # todo make chunk download
 
+MEDIA_TYPES_ALL = ['text', 'photo', 'document', 'audio', 'video', 'voice', 'location', 'sticker']
 
-def identify_media_type(message: Message):
-    media_types = ['photo', 'document', 'audio', 'video', 'voice']
-    for media_type in media_types:
+def identify_media_type(message: Message) -> Optional[str]:
+    for media_type in MEDIA_TYPES_ALL:
         if hasattr(message, media_type):
             if getattr(message, media_type):
                 return media_type
@@ -161,7 +133,6 @@ async def handler_channel_post(update: Update, context: ContextTypes.DEFAULT_TYP
 
     message = update.channel_post
 
-    print('🍎 update_id: ', update.update_id)
     print('🍎', yaml.dump(message, default_flow_style=False))
     print()
 
@@ -170,78 +141,116 @@ async def handler_channel_post(update: Update, context: ContextTypes.DEFAULT_TYP
         return
 
     chat_dir = store / f'chat-{real_chat_id}'
-    task_about_update = update_chat_about_info(message.sender_chat.full_name, chat_dir)
+    pending_task_update_about = update_chat_about_info(message.sender_chat, chat_dir)
 
     now = datetime.now()
     post_dir = chat_dir / f'{now.year}' / f'{now.month:02}'
     post_dir.mkdir(exist_ok=True, parents=True)
 
-    pending_task_download_thumbnail = empty_task()
-    pending_task_download_media_heavy = empty_task()
+    media_type: str = identify_media_type(message)
+    if not media_type:
+        return
 
     context = dict()
 
     context['date'] = message.date
-    context['type'] = 'text'
-    if hasattr(message, 'text'):
+    context['type'] = media_type
+
+    pending_task_download_thumbnail = None
+    pending_task_download_media_heavy = None
+
+    if media_type == 'text':
         if message.text:
             context['text'] = message.text_html
 
+    elif media_type == 'location':
+        if message.location:
+            context['latitude'] = message.location.latitude
+            context['longitude'] = message.location.longitude
 
-    print('📺 Media Type: ')
+    elif media_type in ['photo', 'document', 'audio', 'video', 'voice', 'sticker']:
+        if message.media_group_id:
+            context['media_group_id'] = message.media_group_id
 
-    media_type: str = identify_media_type(message)
-    if media_type:
-        context['type'] = media_type
-        if hasattr(message, 'media_group_id'):
-            if getattr(message, 'media_group_id'):
-                context['media_group_id'] = getattr(message, 'media_group_id')
+        if message.caption:
+            context['caption'] = message.caption_html
 
         media_obj = getattr(message, media_type)
 
-        attributes = [
-            'file_size',
-            'file_name',
-            'title',
-            'height',
-            'width',
-            'duration',
-        ]
-        for attr in attributes:
+        if media_type == 'photo' and isinstance(media_obj, tuple):
+            media_obj = media_obj[-1]
+
+        for attr in ['file_name', 'file_size', 'title', 'height', 'width', 'duration']:
             if hasattr(media_obj, attr):
                 context[attr] = getattr(media_obj, attr)
 
-        ext = get_extension(media_obj) if media_type != 'photo' else'.jpg'
-        print('EXT: ', ext)
-
-        media_path = post_dir / f'{message.message_id}-{media_type}{ext}'
-        context['path'] = media_path.as_posix()
-
-        if hasattr(message, 'caption'):
-            if message.caption:
-                context['caption'] = message.caption_html
-
         if hasattr(media_obj, 'thumbnail'):
-            thumb_cnt, pending_task_download_thumbnail = await make_thumbnail(
-                media_obj.thumbnail, post_dir, message.message_id)
-            context.update({key: thumb_cnt[key] for key in thumb_cnt})
+            try:
+                thumb_file = await media_obj.thumbnail.get_file()
+                thumb_path = post_dir / f'{message.message_id}-thumbnail{pathlib.Path(thumb_file.file_path).suffix}'
 
-        pending_task_download_media_heavy = make_file_download(
-            media_type, media_obj, media_path)
+                pending_task_download_thumbnail = thumb_file.download_to_drive(thumb_path)
+
+                context['thumbnail_file_size'] = media_obj.thumbnail.file_size
+                context['thumbnail_height'] = media_obj.thumbnail.height
+                context['thumbnail_width'] = media_obj.thumbnail.width
+                context['thumbnail_path'] = thumb_path.as_posix()
+            except Exception as e:
+                print('Error', e)
+
+        if ext := await get_extension_media_heavy_object(media_type, media_obj):
+            media_path = post_dir / f'{message.message_id}-{media_type}{ext}'
+            context['path'] = media_path.as_posix()
+
+            pending_task_download_media_heavy = make_file_download(media_obj, media_path)
+
+    if message.forward_origin:
+        print()
+        print('👺 FORAWARD: ')
+        pprint.pprint(message.forward_origin)
+        forward = message.forward_origin
+        context['forward_date'] = forward.date
+
+        sender = None
+        if forward.type == forward.CHANNEL:
+            context['forward_type'] = 'channel'
+            sender = forward.chat
+
+        elif forward.type == forward.USER:
+            context['forward_type'] = 'user'
+            sender = forward.sender_user
+
+        else:
+            context['forward_type'] = 'undefined'
+
+        if sender:
+            if hasattr(sender, 'id'):
+               context['forward_chat_id'] = sender.id
+
+            if hasattr(sender, 'title') and sender.title:
+                context['forward_chat_title'] = sender.title
+
+            if hasattr(sender, 'username') and sender.username:
+                context['forward_chat_username'] = sender.username
+
+            if hasattr(sender, 'first_name') and sender.first_name:
+                context['forward_chat_first_name'] = sender.first_name
+
+            if hasattr(sender, 'last_name') and sender.last_name:
+                context['forward_chat_last_name'] = sender.last_name
 
 
     with post_dir.joinpath(f'{message.message_id}.yaml').open('w') as f:
         yaml.dump(context, f, allow_unicode=True, default_flow_style=False, sort_keys=False)
 
-    if media_type:
-        print('🛰 Task Thumbnail')
+    if pending_task_download_thumbnail:
         await asyncio.create_task(pending_task_download_thumbnail)
 
-        print('🛰 Task Heavy')
+    if pending_task_download_media_heavy:
         await asyncio.create_task(pending_task_download_media_heavy)
 
-    print('🛰 Task About Update')
-    await asyncio.create_task(task_about_update)
+    if pending_task_update_about:
+        await asyncio.create_task(pending_task_update_about)
 
 
 
